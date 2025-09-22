d38ce703d9b46b655fae40e1dd716247067fff17
sa-dd <asaad5436@gmail>
2025-09-23 00:22:38 +0500

    feat:working versions

--- discord/bot.go
+++ discord/bot.go
@@ -2,9 +2,6 @@ package discord
 
 import (
 	"log"
-	"os"
-	"os/signal"
-	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -22,11 +19,6 @@ func Initiate() (*discordgo.Session, error){
 	}
 
 	log.Println("Bot is now running. Press CTRL-C to exit.")
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	<-sc
-
-	dg.Close()
 
 	return dg, err
 
