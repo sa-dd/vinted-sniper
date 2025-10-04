82866222c4d3499c67fb5ae3fa8e025e02bedf75
sa-dd <asaad5436@gmail>
2025-10-04 18:14:21 +0500

    chore: remove redundant messages and logs

--- discord/sendmessage.go
+++ discord/sendmessage.go
@@ -8,13 +8,10 @@ import (
 
 func SendNotif(dg *discordgo.Session, embeds []*discordgo.MessageEmbed, channelID string) {
 
-	dg.ChannelMessageSend(channelID, "NiggaThrum")
-	//log.Println("Sending : ",  len(embeds))
 	if len(embeds) < 10 {
 		_, err := dg.ChannelMessageSendEmbeds(channelID, embeds)
 		if err != nil {
 			log.Println("Error sending Embeds: ", err)
 		}
 	}
-	//log.Println(mg)
 }
